3834dc0d215f2c21747eac4726e6e34f4fe012b2
J <smyshlenyj@gmail.com>
2022-10-28 23:03:28 +0300

    * align

--- 7.7  Container/IntegerArray.cpp
+++ 7.7  Container/IntegerArray.cpp
@@ -19,186 +19,186 @@ public:
 	}
 };
 
-	IntegerArray::IntegerArray(int count) : _count(count)
+IntegerArray::IntegerArray(int count) : _count(count)
+{
+
+	if (count < 0)
 	{
-
-		if (count < 0)
-		{
-			throw BadLength();
-		}
-
-		array = new int[count];
+		throw BadLength();
 	}
 
-	void IntegerArray::InsertElementByIndex(int index, int newElement)
+	array = new int[count];
+}
+
+void IntegerArray::InsertElementByIndex(int index, int newElement)
+{
+	if (index < 0 || index > _count)
 	{
-		if (index < 0 || index > _count)
-		{
-			throw BadRange();
-		}
-
-		_count += 1;
-		int* tempArray = new int[_count];
-
-		for (int i = 0; i < index; ++i)
-		{
-			tempArray[i] = array[i];
-		}
-
-		tempArray[index] = newElement;
-
-		for (int i = index + 1; i < _count; ++i)
-		{
-			tempArray[i] = array[i - 1];
-		}
-
-		delete[] array;
-		array = tempArray;
+		throw BadRange();
 	}
 
-	void IntegerArray::DeleteElementByIndex(int index)
+	_count += 1;
+	int* tempArray = new int[_count];
+
+	for (int i = 0; i < index; ++i)
 	{
-		if (index < 0 || index > _count)
-		{
-			throw BadRange();
-		}
-
-		_count -= 1;
-		int* tempArray = new int[_count];
-		for (int i = 0; i < index; ++i)
-		{
-			tempArray[i] = array[i];
-		}
-
-		for (int i = index; i < _count; ++i)
-		{
-			tempArray[i] = array[i + 1];
-		}
-
-		delete[] array;
-		array = tempArray;
+		tempArray[i] = array[i];
 	}
 
-	const int IntegerArray::GetElementbyIndex(int index) const
+	tempArray[index] = newElement;
+
+	for (int i = index + 1; i < _count; ++i)
 	{
-		return array[index];
+		tempArray[i] = array[i - 1];
 	}
 
-	const int IntegerArray::GetLength() const
+	delete[] array;
+	array = tempArray;
+}
+
+void IntegerArray::DeleteElementByIndex(int index)
+{
+	if (index < 0 || index > _count)
 	{
-		return _count;
+		throw BadRange();
 	}
 
-	void IntegerArray::SetSizeOfArray(int newSize)
+	_count -= 1;
+	int* tempArray = new int[_count];
+	for (int i = 0; i < index; ++i)
 	{
-		if (newSize < 0)
-		{
-			throw BadLength();
-		}
-
-		int* tempArray = new int[newSize];
-
-		for (int i = 0; i < newSize; ++i)
-		{
-			tempArray[i] = array[i];
-		}
-
-		delete[] array;
-
-		array = tempArray;
-		_count = newSize;
+		tempArray[i] = array[i];
 	}
 
-	void IntegerArray::PushBack(int newElement)
+	for (int i = index; i < _count; ++i)
 	{
-		if (newElement < INT_MIN || newElement > INT_MAX) //hmmm....
-		{
-			throw BadRange(); // no matter
-		}
-
-		SetSizeOfArray(_count + 1);
-		array[_count - 1] = newElement;
+		tempArray[i] = array[i + 1];
 	}
 
-	void IntegerArray::PushFront(int newElement)
+	delete[] array;
+	array = tempArray;
+}
+
+const int IntegerArray::GetElementbyIndex(int index) const
+{
+	return array[index];
+}
+
+const int IntegerArray::GetLength() const
+{
+	return _count;
+}
+
+void IntegerArray::SetSizeOfArray(int newSize)
+{
+	if (newSize < 0)
 	{
-		_count += 1;
-		int* tempArray = new int[_count];
-		tempArray[0] = newElement;
-
-		for (int i = 1; i < _count; ++i)
-		{
-			tempArray[i] = array[i - 1];
-		}
-
-		delete[] array;
-
-		array = tempArray;
+		throw BadLength();
 	}
 
-	void IntegerArray::PopBack()
-	{
-		if (_count < 1)
-		{
-			throw BadLength();
-		}
+	int* tempArray = new int[newSize];
 
-		_count -= 1;
-		SetSizeOfArray(_count);
+	for (int i = 0; i < newSize; ++i)
+	{
+		tempArray[i] = array[i];
 	}
 
-	void IntegerArray::PopFront()
+	delete[] array;
+
+	array = tempArray;
+	_count = newSize;
+}
+
+void IntegerArray::PushBack(int newElement)
+{
+	if (newElement < INT_MIN || newElement > INT_MAX) //hmmm....
 	{
-		if (_count < 1)
-		{
-			throw BadLength();
-		}
-
-		_count -= 1;
-		int* tempArray = new int[_count];
-
-		for (int i = 0; i < _count; ++i)
-		{
-			tempArray[i] = array[i + 1];
-		}
-
-		delete[] array;
-
-		array = tempArray;
+		throw BadRange(); // no matter
 	}
 
-	void IntegerArray::CopyContainerTo(IntegerArray& destinationContainer)
+	SetSizeOfArray(_count + 1);
+	array[_count - 1] = newElement;
+}
+
+void IntegerArray::PushFront(int newElement)
+{
+	_count += 1;
+	int* tempArray = new int[_count];
+	tempArray[0] = newElement;
+
+	for (int i = 1; i < _count; ++i)
 	{
-
-		if (destinationContainer._count != _count)
-		{
-			throw BadLength();
-		}
-
-		for (int i = 0; i < _count; ++i)
-		{
-			destinationContainer[i] = array[i];
-		}
+		tempArray[i] = array[i - 1];
 	}
 
-	void IntegerArray::PrintContainer()
+	delete[] array;
+
+	array = tempArray;
+}
+
+void IntegerArray::PopBack()
+{
+	if (_count < 1)
 	{
-		std::cout << "Count " << _count << std::endl;
-		for (int i{ 0 }; i < _count; ++i)
-			std::cout << array[i] << '\n';
+		throw BadLength();
 	}
 
-	int& IntegerArray::operator[](int index)
-	{
-		if (index < 0 || index > _count)
-		{
-			throw BadRange();
-		}
+	_count -= 1;
+	SetSizeOfArray(_count);
+}
 
-		return array[index];
+void IntegerArray::PopFront()
+{
+	if (_count < 1)
+	{
+		throw BadLength();
 	}
 
-	IntegerArray::~IntegerArray()
+	_count -= 1;
+	int* tempArray = new int[_count];
+
+	for (int i = 0; i < _count; ++i)
 	{
-		delete[] array;
+		tempArray[i] = array[i + 1];
 	}
+
+	delete[] array;
+
+	array = tempArray;
+}
+
+void IntegerArray::CopyContainerTo(IntegerArray& destinationContainer)
+{
+
+	if (destinationContainer._count != _count)
+	{
+		throw BadLength();
+	}
+
+	for (int i = 0; i < _count; ++i)
+	{
+		destinationContainer[i] = array[i];
+	}
+}
+
+void IntegerArray::PrintContainer()
+{
+	std::cout << "Count " << _count << std::endl;
+	for (int i{ 0 }; i < _count; ++i)
+		std::cout << array[i] << '\n';
+}
+
+int& IntegerArray::operator[](int index)
+{
+	if (index < 0 || index > _count)
+	{
+		throw BadRange();
+	}
+
+	return array[index];
+}
+
+IntegerArray::~IntegerArray()
+{
+	delete[] array;
+}
